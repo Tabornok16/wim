e0b1670804220ddf559c9be232a637551116b6e0
Jaycey Velandres <155590932+Tabornok16@users.noreply.github.com>
2025-05-13 13:35:18 +0800

    Create LaravelPackageServiceProvider.php

---
+++ src/LaravelPackageServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Wim\LaravelPackage;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Config;
+
+class LaravelPackageServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        // Register package services here
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/laravel-package.php', 'laravel-package'
+        );
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        // Bootstrap package services here
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/laravel-package.php' => \config_path('laravel-package.php'),
+            ], 'config');
+        }
+    }
+} 
